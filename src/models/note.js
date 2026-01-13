0be5c7a54a3bf7990b5d306df763b92561330d4f
Anatoliy Podyryako <podyryakoanatoliy@gmail.com>
2026-01-13 14:05:14 +0200

    completed task

---
+++ src/models/note.js
@@ -0,0 +1,40 @@
+import { Schema } from 'mongoose';
+import { model } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      // required: false,
+      type: String,
+      default: '',
+      trim: true,
+    },
+    tag: {
+      type: String,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Health',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    // versionKey: false,
+  },
+);
+
+export const Note = model('Note', noteSchema);
